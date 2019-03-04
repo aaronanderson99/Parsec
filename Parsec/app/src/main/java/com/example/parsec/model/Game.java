08a945257a44b164d0906b1f0c13e217be5733f3
Aaron Anderson <aanderson99@gatech.edu>
2019-03-04 18:22:16 -0500

    Added Ship to Game

--- Parsec/app/src/main/java/com/example/parsec/model/Game.java
+++ Parsec/app/src/main/java/com/example/parsec/model/Game.java
@@ -1,5 +1,8 @@
 package com.example.parsec.model;
 
+/**
+ * Singleton class that holds game state
+ */
 public class Game {
 
     private Player player;
@@ -7,15 +10,47 @@ public class Game {
     private Universe universe;
     private Difficulty difficulty;
 
-    public Game() {
+    private static Game game = new Game();
+
+    /**
+     * Default constructor
+     */
+    private Game() {
         this(new Player(), new Ship(), new Universe(), Difficulty.Easy);
     }
 
     public Game(Player player, Ship ship, Universe universe, Difficulty difficulty) {
         this.player = player;
         this.ship = ship;
-        this.universe = universe;
+        this.universe = universe.generateDefaultUniverse();
         this.difficulty = difficulty;
     }
 
+    public static Game getInstance() {
+        return game;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Universe getUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(Universe universe) {
+        this.universe = universe;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
 }
