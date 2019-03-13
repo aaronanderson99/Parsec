06ebc308fab3fc9ab9f35c2d5d2945aa66bf8532
Aiza Shabbir <aizashabbir@lawn-128-61-78-104.lawn.gatech.edu>
2019-03-13 18:01:38 -0400

    added UI

--- Parsec/app/src/main/java/com/example/parsec/views/SystemActivity.java
+++ Parsec/app/src/main/java/com/example/parsec/views/SystemActivity.java
@@ -1,20 +1,62 @@
 package com.example.parsec.views;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.parsec.R;
 
-public class SystemActivity extends AppCompatActivity {
+public class SystemActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_system);
+
+        bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new SystemInfoFragment());
     }
 
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch(item.getItemId()) {
+            case R.id.navigation_system_info:
+                fragment = new SystemInfoFragment();
+                break;
+
+            case R.id.navigation_marketplace:
+                fragment = new MarketplaceFragment();
+                break;
+
+            case R.id.navigation_space_port:
+                fragment = new SpaceportFragment();
+                break;
+        }
+        return loadFragment(fragment);
+    }
 }
